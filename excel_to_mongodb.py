90a6d90e778b025b4e514e75b170d5c997859ad7
Apoc9512 <57773353+Apoc9512@users.noreply.github.com>
2023-07-12 05:03:11 -0700

    excel_to_mongodb
    V 1.2

--- excel_to_mongodb.py
+++ excel_to_mongodb.py
@@ -1,108 +1,99 @@
-import argparse
 import pandas as pd
+import argparse
 from pymongo import MongoClient
 
-# MongoDB configuration
-MONGO_HOST = 'localhost'
-MONGO_PORT = 27017
+# Config
+HOST = 'localhost' 
+PORT = 27017
 
-def import_records(file, database, collection, sor, date, sheet_name):
+def import_records(file, db_name, coll_name, sor, date, sheet_name):
 
-    df = pd.read_excel(file, sheet_name=sheet_name)
+  df = pd.read_excel(file, sheet_name=sheet_name)
+  records = df.to_dict(orient='records')
 
-    records = df.to_dict(orient='records')
+  client = MongoClient(HOST, PORT)
+  db = client[db_name]
+  coll = db[coll_name]
 
-    # Connect to MongoDB
-    client = MongoClient(MONGO_HOST, MONGO_PORT)
-    db = client[database]
-    coll = db[collection]
-    coll.insert_many(records)
+  coll.insert_many(records)
+  coll.update_many({}, {'$set': {'SOR': sor, 'DATE': date}})
 
-    # Update SOR and DATE fields in each record
-    coll.update_many({}, {"$set": {"SOR": sor, "DATE": date}})
+  print(f'Imported {len(records)} records')
 
-    print(f"Successfully imported {len(records)} records into MongoDB collection '{collection}'")
+def update_records(file, db_name, coll_name, sor, date, sheet_name, output_file):
+  
+  df = pd.read_excel(file, sheet_name=sheet_name)
+  df['NAME'] = df['NAME'].astype(str)
 
-def compare_and_update_records(file, database, collection, sor, date, sheet_name):
-    # Load excel/read
-    df = pd.read_excel(file, sheet_name=sheet_name)
+  docs = df.to_dict(orient='records')
 
-    # Convert
-    new_records = df.to_dict(orient='records')
+  client = MongoClient(HOST, PORT)
+  db = client[db_name]
+  coll = db[coll_name]
 
-    # Connect to MongoDB
-    client = MongoClient(MONGO_HOST, MONGO_PORT)
-    db = client[database]
-    coll = db[collection]
+  added = []
+  removed = []
 
-    existing_records = coll.find()
+  for doc in docs:
+    name = doc['NAME']
+    if not isinstance(name, str):
+      print(f'Invalid name: {name}')
+      continue
 
-    existing_names = set(record['NAME'] for record in existing_records)
+    if coll.count_documents({'NAME': name}) > 0:
+      coll.update_one({'NAME': name}, {'$set': doc})
+    else:
+      coll.insert_one({**doc, 'SOR': sor, 'DATE': date})
+      added.append(name)
 
-    # Compare and update records
-    updated_count = 0
-    added_names = []
-    removed_names = []
-    for record in new_records:
-        name = record['NAME']
-        if name in existing_names:
-            coll.update_one({"NAME": name}, {"$set": {"DATE": date, **record}})
-            updated_count += 1
-        else:
-            coll.insert_one({**record, "SOR": sor, "DATE": date})
-            added_names.append(name)
+  removed = [n for n in coll.distinct('NAME') if n not in [d['NAME'] for d in docs]]
 
-    coll.delete_many({"NAME": {"$nin": [record['NAME'] for record in new_records]}})
-    removed_names = existing_names - set(record['NAME'] for record in new_records)
+  report = {
+    'Total': len(docs),
+    'Updated': len(docs) - len(added),
+    'Added': added, 
+    'Removed': removed
+  }
 
-    updated_names = set(record['NAME'] for record in new_records) & existing_names
+  if output_file:
+    with open(output_file, 'w') as f:
+      for k, v in report.items():
+        try:
+          v = ', '.join(v) 
+        except TypeError:
+          v = str(v)
+        f.write(f'{k}: {v}\n')
 
-    print(f"Successfully updated {updated_count} records in MongoDB collection '{collection}'")
+  else:  
+    print(report)
 
-    # Convert added_names and removed_names to strings
-    added_names = [str(name) for name in added_names]
-    removed_names = [str(name) for name in removed_names]
-
-    # Generate report
-    report = {
-        'Total Records': len(new_records),
-        'Updated Records': updated_count,
-        'Unchanged Records': len(new_records) - updated_count,
-        'Added Names': added_names,
-        'Removed Names': removed_names
-    }
-
-    print("\nReport:")
-    for key, value in report.items():
-        if isinstance(value, list):
-            value = ', '.join(value)
-        print(f"{key}: {value}")
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Import or update records in MongoDB from an Excel file')
-    subparsers = parser.add_subparsers(dest='command')
+  
+  parser = argparse.ArgumentParser()
+  subparsers = parser.add_subparsers(dest='command')
 
-    # Import command
-    import_parser = subparsers.add_parser('import', help='Import records into MongoDB from an Excel file')
-    import_parser.add_argument('-f', '--file', required=True, help='Path to the Excel file')
-    import_parser.add_argument('-d', '--database', required=True, help='Name of the MongoDB database')
-    import_parser.add_argument('-c', '--collection', required=True, help='Name of the MongoDB collection')
-    import_parser.add_argument('-s', '--sor', required=True, help='Value for the SOR field')
-    import_parser.add_argument('-dt', '--date', required=True, help='Value for the DATE field')
-    import_parser.add_argument('-sn', '--sheet_name', default=0, help='Name or index of the sheet in the Excel file')
+  import_parser = subparsers.add_parser('import')
+  import_parser.add_argument('-f', required=True)
+  import_parser.add_argument('-d', required=True)
+  import_parser.add_argument('-c', required=True)
+  import_parser.add_argument('-s', required=True)
+  import_parser.add_argument('-dt', required=True)
+  import_parser.add_argument('-sn', default=0)
 
-    # Update command
-    update_parser = subparsers.add_parser('update', help='Update records in MongoDB from an Excel file')
-    update_parser.add_argument('-f', '--file', required=True, help='Path to the Excel file')
-    update_parser.add_argument('-d', '--database', required=True, help='Name of the MongoDB database')
-    update_parser.add_argument('-c', '--collection', required=True, help='Name of the MongoDB collection')
-    update_parser.add_argument('-s', '--sor', required=True, help='Value for the SOR field')
-    update_parser.add_argument('-dt', '--date', required=True, help='Value for the DATE field')
-    update_parser.add_argument('-sn', '--sheet_name', default=0, help='Name or index of the sheet in the Excel file')
+  update_parser = subparsers.add_parser('update')
+  update_parser.add_argument('-f', required=True)
+  update_parser.add_argument('-d', required=True)
+  update_parser.add_argument('-c', required=True)
+  update_parser.add_argument('-s', required=True)
+  update_parser.add_argument('-dt', required=True)
+  update_parser.add_argument('-sn', default=0)
+  update_parser.add_argument('-o', required=False)
 
-    args = parser.parse_args()
+  args = parser.parse_args()
 
-    if args.command == 'import':
-        import_records(args.file, args.database, args.collection, args.sor, args.date, args.sheet_name)
-    elif args.command == 'update':
-        compare_and_update_records(args.file, args.database, args.collection, args.sor, args.date, args.sheet_name)
+  if args.command == 'import':
+    import_records(args.f, args.d, args.c, args.s, args.dt, args.sn)
+
+  elif args.command == 'update':
+    update_records(args.f, args.d, args.c, args.s, args.dt, args.sn, args.o)
